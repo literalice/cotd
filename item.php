c4a51c72b79459563f08db1acf50ef7f083fe5f3
Masatoshi Hayashi <literalice@monochromeroad.com>
2018-01-24 10:40:43 +0900

    Update item.php

--- item.php
+++ item.php
@@ -87,7 +87,7 @@ $help = $help."<p style='color:silver;font-size:80%;font-style:italic'> Enjoy, S
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>COTD v2</title>
+	<title>COTD v3</title>
 	<!-- <link rel="stylesheet"  href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css"> -->
 	<link rel="stylesheet"  href="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
 	<link rel="stylesheet" href="css/jqm-demos.css">
